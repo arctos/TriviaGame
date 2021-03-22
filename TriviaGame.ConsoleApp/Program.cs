ab407ff2519bf6b145a0dd0852770b04674dea66
Justin Baker <me@jbaker.dev>
2021-03-22 15:50:27 -0700

    Create solution

---
+++ TriviaGame.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TriviaGame.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
